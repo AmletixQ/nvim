418846a6ddd2ceec9e3a960b6afadb8477c595ee
AmletixQ <nazarovamridin308@gmail.com>
2024-11-18 20:36:20 +0300

    Complete configure the none-ls and tree-sitter for work with cpp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "csharp_ls" }
+        ensure_installed = { "lua_ls", "clangd", },
       })
     end,
   },
@@ -17,10 +17,13 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
-  }
+  },
 }
